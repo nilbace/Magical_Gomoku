f044e2fcdadd2d7d1bf6bb756984a3743e91b9b6
nilbace <nilbace@naver.com>
2022-10-28 23:37:15 +0900

    Lobby Done
    방 입장까지 구현 완료

--- My project/Assets/Scripts/NetWorkManager.cs
+++ My project/Assets/Scripts/NetWorkManager.cs
@@ -29,6 +29,7 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
     public GameObject StartScreen;
     public TextMeshProUGUI status; 
     public TMP_InputField nameField;
+    public Button ConnectBtn;
     bool isStart = false;
     
 
@@ -38,18 +39,36 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
     public TextMeshProUGUI welcome;
     public TextMeshProUGUI clientNumber;
     bool isLobby = false;
-    
+    [Header("RoomList")]
+    public Button[] PNrooms;
+    public TextMeshProUGUI[] RoomNames;
+    public Button previousPage; public Button nextPage;
+    List<RoomInfo> myList = new List<RoomInfo>();
+    public int currentPage = 1, maxPage = 1, multiple = 5;
+
+    [Header("Room")]
+    public GameObject GamePannel;
+    bool isGaming = false;
+
+    void test()
+    {
+        
+    }
 
     void Start()
     {
         Screen.SetResolution(960, 540, false);
         PhotonNetwork.ConnectUsingSettings();
+        StartScreen.SetActive(true);
+        LobbyScreen.SetActive(false);
         isStart = true;
+        ConnectBtn.interactable = false;
     }
 
     public override void OnConnectedToMaster()
     {
         status.color= Color.green;
+        ConnectBtn.interactable = true;
     }
 
     public override void OnDisconnected(DisconnectCause cause)
@@ -62,7 +81,7 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.LocalPlayer.NickName = nameField.text;
         PhotonNetwork.JoinLobby();
         StartScreen.SetActive(false);
-        
+        myList.Clear();
     }
    
     public override void OnJoinedLobby()
@@ -71,10 +90,64 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
         LobbyScreen.SetActive(true);
     }
 
-    public void CreateRoom() => 
+    public void CreateRoom() 
+    {
+        if(roomnameField.text == "") 
+        {
+            print("Please Enter room Name");
+            return;
+        }
         PhotonNetwork.CreateRoom(roomnameField.text, new RoomOptions{MaxPlayers=2}, null);
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        print("another Room name required");
+    }
 
-    
+    #region 방목록갱신
+    public void MyListClick(int num)
+    {
+        if(num == -2) --currentPage;
+        else if(num == -1) ++currentPage;
+        else PhotonNetwork.JoinRoom(myList[multiple+num].Name);
+    }
+
+    public void MyListRenewal()
+    {
+        previousPage.interactable = (currentPage == 2);
+        nextPage.interactable = (currentPage == 1);
+
+        multiple = (currentPage-1)*5;
+        for(int i =0;i<5;i++)
+        {
+            PNrooms[i].interactable = (multiple+i<myList.Count)?true:false;
+            RoomNames[i].text = (multiple + i < myList.Count) ? myList[multiple+i].Name : "";
+        }
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        int roomCount = roomList.Count;
+        for(int i = 0;i<roomCount;i++)
+        {
+            if(!roomList[i].RemovedFromList)
+            {
+                if(!myList.Contains(roomList[i])) myList.Add(roomList[i]);
+                else myList[myList.IndexOf(roomList[i])] = roomList[i];
+            }
+            else if(myList.IndexOf(roomList[i])!= -1)myList.RemoveAt(myList.IndexOf(roomList[i]));
+            MyListRenewal();
+        }
+    }
+    #endregion
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        PV.RPC("EnteredRoom", RpcTarget.All, newPlayer);
+    }
+    [PunRPC] void EnteredRoom(Player otherPlayer)
+    {
+        print(otherPlayer.NickName+"님이 입장하셨습니다.");
+    }
     void Update()
     {   if(SceneManager.GetActiveScene().buildIndex==0)
         status.text = PhotonNetwork.NetworkingClient.State.ToString();
@@ -84,6 +157,10 @@ public class NetWorkManager : MonoBehaviourPunCallbacks
             clientNumber.text = (PhotonNetwork.CountOfPlayers - PhotonNetwork.CountOfPlayersInRooms) +
             "Lobby / " + PhotonNetwork.CountOfPlayers + "Connected";
             welcome.text = PhotonNetwork.LocalPlayer.NickName + "  Welcome";
+            for(int i = 0;i<myList.Count;i++)
+            {
+                RoomNames[i].text = myList[i].Name;
+            }
         }
     }
 }
